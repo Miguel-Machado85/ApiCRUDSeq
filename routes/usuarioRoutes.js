12e21fdc68294abe3229fe0d8819c0d9ba719ded
Miguel-Machado85 <miguel.machado8506@gmail.com>
2025-03-05 10:20:06 -0500

    (Feat) metodos CRUD, migraciones

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const usuarioController = require("../controller/usuarioController");
 
 router.get("/",usuarioController.getUsers);
+router.post("/",usuarioController.addUser);
+router.put("/:id",usuarioController.updateUser);
+router.put("/cambiarEstado/:id",usuarioController.changeUserStatus);
 
 module.exports = router;
